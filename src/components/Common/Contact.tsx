a00a401d55b4a3ea9b39f929288385fc707fb2cf
duhdanube <Danny.B.Lim@gmail.com>
2020-11-03 10:45:29 +0700

    Package option for Contact form

--- src/components/Common/Contact.tsx
+++ src/components/Common/Contact.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Form, Input, Button, Typography, } from 'antd'
+import React, { useState, ReactText, } from 'react'
+import { Form, Input, Button, Typography, Dropdown, Menu, } from 'antd'
+import { DownOutlined, FormatPainterOutlined, CodeOutlined, PartitionOutlined, } from '@ant-design/icons'
 
 const { Title, } = Typography
 
@@ -17,10 +18,37 @@ const validateMessages = {
 	},
 }
 
+interface MenuClickEvent {
+	domEvent: any
+	item: any
+	key: ReactText
+	keyPath: ReactText[]
+}
+
 export default function Contact(): JSX.Element {
+	const [packageChoice, setPackageChoice] = useState<"Web & App Design" | "Website & App Development" | "Design & IT Consulting" | "Packages">("Packages")
+
 	const onFinish = (values: { user: { email: string, message: string, name: string, website: string, } }) => {
 		console.log(values)
 	}
+
+	const handlePackagePick = (info: MenuClickEvent): void => {
+		setPackageChoice(info.key.toString() as "Web & App Design" | "Website & App Development" | "Design & IT Consulting" | "Packages")
+	}
+
+	const menu = (
+		<Menu onClick={handlePackagePick}>
+			<Menu.Item key="Web & App Design" icon={<FormatPainterOutlined />}>
+				Web & App Design
+    	</Menu.Item>
+			<Menu.Item key="Website & App Development" icon={<CodeOutlined />}>
+				Website & App Development
+    	</Menu.Item>
+			<Menu.Item key="Design & IT Consulting" icon={<PartitionOutlined />}>
+				Design & IT Consulting
+    	</Menu.Item>
+		</Menu>
+	)
 
 	return (
 		<div style={{ width: '100%', display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', }}>
@@ -34,6 +62,13 @@ export default function Contact(): JSX.Element {
 				</Form.Item>
 				<Form.Item name={['user', 'website']} label="Website">
 					<Input />
+				</Form.Item>
+				<Form.Item name={['user', 'package']} label="Package">
+					<Dropdown overlay={menu} className="contact-package-button">
+						<Button>
+							{packageChoice === "Web & App Design" ? <FormatPainterOutlined /> : packageChoice === "Website & App Development" ? <CodeOutlined /> : packageChoice === "Design & IT Consulting" ? <PartitionOutlined /> : <></>} {packageChoice} {packageChoice === "Packages" && <DownOutlined />}
+						</Button>
+					</Dropdown>
 				</Form.Item>
 				<Form.Item name={['user', 'message']} label="Message" rules={[{ required: true, }]}>
 					<Input.TextArea />
