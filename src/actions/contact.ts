954edbb0cd3c4fde271b84177dc02cfe177d0234
duhdanube <Danny.B.Lim@gmail.com>
2020-11-04 16:40:21 +0700

    Finished Email SMTP Server work

--- src/actions/contact.ts
+++ src/actions/contact.ts
@@ -1,40 +1,42 @@
-const sendMessage = async (name: string, email: string, message: string, website: string, servicePackage: string): Promise<any> => {
-	if (process.env.REACT_APP_SERVER_URL) {
-		fetch(`${process.env.REACT_APP_SERVER_URL}/contact` || '', {
-			method: 'POST',
-			mode: 'cors',
-			cache: 'no-cache',
-			credentials: 'same-origin',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			redirect: 'follow',
-			referrerPolicy: 'no-referrer',
-			body: JSON.stringify({
-				name, email, message, website, servicePackage,
-			})
-		}).then(() => {
-			return 'Success'
-		}).catch((reason) => {
-			return reason
+const sendFeedback = (serviceID: string, templateId: string, variables: { from_name: string, from_email: string, from_website: string, message: string, servicePackage: string, }): Promise<any> => {
+	return new Promise((resolve, reject) => {
+		(window as any).emailjs.send(
+			serviceID, templateId,
+			variables
+		).then((res: any) => {
+			resolve(res)
+		}).catch((err: any) => {
+			reject(err)
 		})
-	} else {
-		return "Error"
-	}
+	})
+}
+
+const sendMessage = async (name: string, email: string, message: string, website: string, servicePackage: string): Promise<any> => {
+	return new Promise((resolve, reject) => {
+		sendFeedback(process.env.REACT_APP_EMAILJS_SERVICE_ID || '', process.env.REACT_APP_EMAILJS_TEMPLATE_ID || '', { from_name: name, from_email: email, from_website: website, message, servicePackage, }).then(() => {
+			resolve('Success')
+		}).catch((error) => {
+			reject(error)
+		})
+	})
 }
 
 export const sendEmail = async (email: string): Promise<any> => {
-	sendMessage('New Inquiry', email, `New inquiry from ${email}`, '', '').then((response) => {
-		return response
-	}).catch((error: JSON) => {
-		return error
+	return new Promise((resolve, reject) => {
+		sendMessage('New Inquiry', email, `New inquiry from ${email}`, '', '').then((response) => {
+			resolve(response)
+		}).catch((error: JSON) => {
+			reject(error)
+		})
 	})
 }
 
 export const contactAlmega = async (name: string, email: string, message: string, servicePackage: string, website?: string): Promise<any> => {
-	sendMessage(name, email, message, website || '', servicePackage).then((response) => {
-		return response
-	}).catch((error: JSON) => {
-		return error
+	return new Promise((resolve, reject) => {
+		sendMessage(name, email, message, website || '', servicePackage).then((response) => {
+			resolve(response)
+		}).catch((error: JSON) => {
+			reject(error)
+		})
 	})
 }
