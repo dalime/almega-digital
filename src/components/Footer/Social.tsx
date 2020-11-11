b7b8fd7a704ca78c601c767cee35c2ea75f1cea3
duhdanube <Danny.B.Lim@gmail.com>
2020-11-11 14:01:04 +0700

    Social Media Links in footer

---
+++ src/components/Footer/Social.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { InstagramOutlined, TwitterOutlined, LinkedinOutlined, BehanceOutlined, MediumOutlined, } from '@ant-design/icons'
+
+interface Props {
+	mobile?: boolean
+}
+
+export default function Social(props: Props): JSX.Element {
+	const { mobile, } = props
+
+	return (
+		<div style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 40, marginBottom: 40, }}>
+			<InstagramOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_INSTAGRAM || 'https://www.instagram.com', '_blank')} />
+			<TwitterOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_TWITTER || 'https://www.twitter.com', '_blank')} />
+			<LinkedinOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_LINKEDIN || 'https://www.linkedin.com', '_blank')} />
+			<BehanceOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_BEHANCE || 'https://www.behance.net', '_blank')} />
+			<MediumOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_MEDIUM || 'https://wwww.medium.com', '_blank')} />
+		</div>
+	)
+}
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Props, } from '../../types'
 import CallToAction from './CallToAction'
+import Social from './Social'
 
 export default function Footer(props: Props): JSX.Element {
 	const { mobile, } = props
@@ -9,6 +10,7 @@ export default function Footer(props: Props): JSX.Element {
 	return (
 		<div>
 			<CallToAction mobile={mobile} />
+			<Social mobile={mobile} />
 			<div style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 10, }}>
 				{process.env.REACT_APP_SSL_CERTIFICATE_SRC && <span id="siteseal"><script async type="text/javascript" src={process.env.REACT_APP_SSL_CERTIFICATE_SRC || ''}></script></span>}
 				Copyright © {new Date().getFullYear()} Almega Digital. All Rights Reserved.
