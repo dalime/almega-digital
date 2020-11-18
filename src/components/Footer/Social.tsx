85d86cd3461cc2161ec5438e38f29b79568cb4f7
duhdanube <Danny.B.Lim@gmail.com>
2020-11-18 11:48:20 +0700

    Made social media icons smaller

--- src/components/Footer/Social.tsx
+++ src/components/Footer/Social.tsx
@@ -9,12 +9,12 @@ export default function Social(props: Props): JSX.Element {
 	const { mobile, } = props
 
 	return (
-		<div style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', marginTop: 40, marginBottom: 40, }}>
-			<InstagramOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_INSTAGRAM || 'https://www.instagram.com', '_blank')} />
-			<TwitterOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_TWITTER || 'https://www.twitter.com', '_blank')} />
-			<LinkedinOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_LINKEDIN || 'https://www.linkedin.com', '_blank')} />
-			<BehanceOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_BEHANCE || 'https://www.behance.net', '_blank')} />
-			<MediumOutlined style={{ fontSize: mobile ? 35 : 50, color: '#001529', }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_MEDIUM || 'https://wwww.medium.com', '_blank')} />
+		<div style={{ display: 'flex', flex: 1, flexDirection: 'row', justifyContent: 'center', marginTop: 40, marginBottom: 40, }}>
+			<InstagramOutlined style={{ fontSize: mobile ? 25 : 35, color: '#001529', marginLeft: mobile ? 5 : 10, marginRight: mobile ? 5 : 10, }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_INSTAGRAM || 'https://www.instagram.com', '_blank')} />
+			<TwitterOutlined style={{ fontSize: mobile ? 25 : 35, color: '#001529', marginLeft: mobile ? 5 : 10, marginRight: mobile ? 5 : 10, }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_TWITTER || 'https://www.twitter.com', '_blank')} />
+			<LinkedinOutlined style={{ fontSize: mobile ? 25 : 35, color: '#001529', marginLeft: mobile ? 5 : 10, marginRight: mobile ? 5 : 10, }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_LINKEDIN || 'https://www.linkedin.com', '_blank')} />
+			<BehanceOutlined style={{ fontSize: mobile ? 25 : 35, color: '#001529', marginLeft: mobile ? 5 : 10, marginRight: mobile ? 5 : 10, }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_BEHANCE || 'https://www.behance.net', '_blank')} />
+			<MediumOutlined style={{ fontSize: mobile ? 25 : 35, color: '#001529', marginLeft: mobile ? 5 : 10, marginRight: mobile ? 5 : 10, }} onClick={() => window.open(process.env.REACT_APP_SOCIAL_MEDIUM || 'https://wwww.medium.com', '_blank')} />
 		</div>
 	)
 }
