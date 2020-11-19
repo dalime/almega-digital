a2418c6b5e89126effe25314aaf7e56a879c8a6b
duhdanube <Danny.B.Lim@gmail.com>
2020-11-19 19:55:40 +0700

    Optimized images by reducing size, added shadow to header, and spaced out sections more on desktop version

--- src/components/Navigation/Desktop.tsx
+++ src/components/Navigation/Desktop.tsx
@@ -7,7 +7,7 @@ const { Header, } = Layout
 
 export default function Desktop(): JSX.Element {
 	return (
-		<Header style={{ position: 'fixed', zIndex: 1, width: '100%', minHeight: '10vh', }}>
+		<Header style={{ position: 'fixed', zIndex: 16, width: '100%', minHeight: '10vh', }}>
 			<Menu />
 		</Header>
 	)
